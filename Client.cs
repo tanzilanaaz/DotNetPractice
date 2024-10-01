abab353c2dec96649fd46266185bc0fd6a5a0b06
tanzilanaaz <113665972+tanzilanaaz@users.noreply.github.com>
2024-10-01 16:21:17 +0530

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    public class Client
+    {
+        private Packaging _packaging;
+        private DeliveryDocument _deliveryDocument;
+        public Client(PacknDelvFactory factory)
+        {
+            _packaging = factory.CreatePackaging();
+            _deliveryDocument = factory.CreateDeliveryDocument();
+        }
+        public Packaging ClientPackaging
+        {
+            get { return _packaging; }
+        }
+        public DeliveryDocument ClientDocument
+        {
+            get { return _deliveryDocument; }
+        }
+
+    }
+}
---
+++ PacknDelvFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    public abstract class PacknDelvFactory
+    {
+        public abstract Packaging CreatePackaging();
+        public abstract DeliveryDocument CreateDeliveryDocument();
+    }
+    public class StandardFactory : PacknDelvFactory
+    {
+        public override Packaging CreatePackaging()
+        {
+            return new StandardPackaging();
+        }
+        public override DeliveryDocument CreateDeliveryDocument()
+        {
+            return new Postal();
+        }
+    }
+    public class DelicateFactory : PacknDelvFactory
+    {
+        public override Packaging CreatePackaging()
+        {
+            return new ShockProofPackaging();
+        }
+        public override DeliveryDocument CreateDeliveryDocument()
+        {
+            return new Courier();
+        }
+    }
+    public abstract class Packaging { }
+    public class StandardPackaging : Packaging { }
+    public class ShockProofPackaging : Packaging { }
+    public abstract class DeliveryDocument { }
+    public class Postal : DeliveryDocument { }
+    public class Courier : DeliveryDocument { }
+}
